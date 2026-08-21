d9bfa0bbe9ac435aa3b02c94457bba0b4d28906e
Rifki Akbar Z <99713868+akbarzamzami@users.noreply.github.com>
2026-08-21 15:50:37 +0700

    fix: restore Laravel default service providers

--- config/app.php
+++ config/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\ServiceProvider;
+
 return [
 
     /*
@@ -9,7 +11,7 @@ return [
     |
     | This value is the name of your application, which will be used when the
     | framework needs to place the application's name in a notification or
-    | other UI elements where an application name needs to be displayed.
+    | other UI elements where the application's name may be displayed.
     |
     */
 
@@ -21,8 +23,8 @@ return [
     |--------------------------------------------------------------------------
     |
     | This value determines the "environment" your application is currently
-    | running in. This may determine how you prefer to configure various
-    | services the application utilizes. Set this in your ".env" file.
+    | running in. This may be set to any of the environments supported by
+    | Laravel, such as "local", "development", or "production".
     |
     */
 
@@ -33,7 +35,7 @@ return [
     | Application Debug Mode
     |--------------------------------------------------------------------------
     |
-    | When your application is in debug mode, detailed error messages with
+    | When application debug mode is enabled, detailed error messages with
     | stack traces will be shown on every error that occurs within your
     | application. If disabled, a simple generic error page is shown.
     |
@@ -46,9 +48,9 @@ return [
     | Application URL
     |--------------------------------------------------------------------------
     |
-    | This URL is used by the console to properly generate URLs when using
+    | This URL is used by the framework to properly generate URLs when using
     | the Artisan command line tool. You should set this to the root of
-    | the application so that it's available within Artisan commands.
+    | the application so that it is available within Artisan commands.
     |
     */
 
@@ -60,8 +62,8 @@ return [
     |--------------------------------------------------------------------------
     |
     | Here you may specify the default timezone for your application, which
-    | will be used by the PHP date and date-time functions. The timezone
-    | is set to "UTC" by default as it is suitable for most use cases.
+    | will be used by PHP's date and date-time functions. The timezone is set
+    | to UTC by default as it is suitable for most applications.
     |
     */
 
@@ -73,8 +75,8 @@ return [
     |--------------------------------------------------------------------------
     |
     | The application locale determines the default locale that will be used
-    | by Laravel's translation / localization methods. This option can be
-    | set to any locale for which you plan to have translation strings.
+    | by Laravel's translation / localization methods. This option can be set
+    | to any locale for which you plan to provide translation strings.
     |
     */
 
@@ -90,8 +92,8 @@ return [
     |--------------------------------------------------------------------------
     |
     | This key is utilized by Laravel's encryption services and should be set
-    | to a random, 32 character string to ensure that all encrypted values
-    | are secure. You should do this prior to deploying the application.
+    | to a random, 32 character string to ensure that all encrypted values are
+    | secure. You should do this prior to deploying the application.
     |
     */
 
@@ -107,12 +109,28 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Autoloaded Service Providers
+    |--------------------------------------------------------------------------
+    |
+    | The service providers listed here will be automatically loaded on every
+    | request. Laravel's default providers include the ViewServiceProvider
+    | required by Blade and Route::view().
+    |
+    */
+
+    'providers' => ServiceProvider::defaultProviders()->merge([
+        // Package Service Providers...
+    ])->merge([
+        // Application Service Providers are loaded from bootstrap/providers.php.
+    ])->toArray(),
+
+    /*
+    |--------------------------------------------------------------------------
     | Maintenance Mode Driver
     |--------------------------------------------------------------------------
     |
     | These configuration options determine the driver used to determine and
-    | manage Laravel's "maintenance mode" status. The "cache" driver will
-    | allow maintenance mode to be controlled across multiple machines.
+    | manage the application's maintenance mode status.
     |
     | Supported drivers: "file", "cache", "array"
     |
